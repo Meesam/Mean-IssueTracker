dc701c6d83d82de182940a79d69c0e93757d9ff6
meesam <meesam.engineer@gmail.com>
2016-12-01 08:01:40 +0530

    Search Ui added

--- api/elasticSearchapi.js
+++ api/elasticSearchapi.js
@@ -7,7 +7,8 @@ let elasticSearch=require('../controllers/elasticSearchmodule/elasticSearch');
 let apiRoutes = express.Router();
 
 /* GET Name */
-apiRoutes.get('/name/:input', function (req, res, next) {  
+apiRoutes.get('/searchAccount/:input', function (req, res, next) {  
+  console.log('input is ' + req.params.input);
   elasticSearch.getSuggestions(req.params.input)
     .then(function(data){
     	res.json(data);
